3d1121bff9560808baa7cda430a876830fabf2b1
chriscauley <chris@lablackey.com>
2021-01-31 13:02:19 +0000

    adding vue bootstrap from vue cli

---
+++ vue-client/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>types: {{ slugs.join(', ') }}</div>
+</template>
+
+<script>
+import types from 'tw/piece/types'
+export default {
+  runtimeCompiler: true,
+  name: 'App',
+  data() {
+    return { slugs: types.slugs }
+  },
+}
+</script>
+
+<style></style>
