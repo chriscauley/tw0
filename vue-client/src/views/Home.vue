0eea75d5c5d5627c3d9566202e4a2a1b973573dc
chriscauley <chris@lablackey.com>
2021-02-03 00:07:14 +0000

    adding links to home

--- vue-client/src/views/Home.vue
+++ vue-client/src/views/Home.vue
@@ -1,11 +1,17 @@
 <template>
-  <div>todo: home page</div>
+  <div>
+    <div v-for="link in links" :key="link">
+      <router-link :to="link">{{ link }}</router-link>
+    </div>
+  </div>
 </template>
 
 <script>
+const links = ['/docs/css/', '/sprite/list/', '/coloseum/9x9/bat/']
 export default {
   __route: {
     path: '/',
   },
+  data: () => ({ links }),
 }
 </script>
