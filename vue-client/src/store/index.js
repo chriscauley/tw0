3bce1d807bab62c23a7fbee59cbcb9be920c9dfd
chriscauley <chris@lablackey.com>
2021-02-07 14:22:35 +0000

    creating means to export sprite data (no import yet)

--- vue-client/src/store/index.js
+++ vue-client/src/store/index.js
@@ -12,4 +12,16 @@ store.init = () => {
   store.list.forEach((m) => m.init(m))
 }
 
+window.exportJson = () => {
+  ;['sprite/sheet', 'sprite/tag'].forEach((slug) => {
+    const LS_KEY = 'store/' + slug
+    const fname = slug.replace(/\//g, '__') + '.json'
+    const a = document.createElement('a')
+    const text = window.localStorage.getItem(LS_KEY)
+    a.href = window.URL.createObjectURL(new window.Blob([text], { type: 'file text' }))
+    a.download = fname
+    a.click()
+  })
+}
+
 export default store
