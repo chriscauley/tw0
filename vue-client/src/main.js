e57a142792c2db9a867d07cbf586cc97e791cf18
chriscauley <chris@lablackey.com>
2021-02-08 00:57:24 +0000

    short cut for stats

--- vue-client/src/main.js
+++ vue-client/src/main.js
@@ -5,6 +5,7 @@ import App from './App.vue'
 import Vacuform from './vacuform'
 import Popper from '@/components/Popper'
 import router from './router'
+import Stats from 'stats.js'
 
 import './styles/base.scss'
 import '@unrest/tailwind/dist.css'
@@ -19,3 +20,19 @@ function main() {
 }
 
 main()
+
+// TODO v-hotkey
+const doStats = (event) => {
+  if (event.shiftKey && event.ctrlKey && event.key === 'S') {
+    const stats = new Stats()
+    stats.showPanel(0) // 0: fps, 1: ms, 2: mb, 3+: custom
+    document.body.appendChild(stats.dom)
+    requestAnimationFrame(function loop() {
+      stats.update()
+      requestAnimationFrame(loop)
+    })
+    document.removeEventListener('keypress', doStats)
+  }
+}
+
+document.addEventListener('keypress', doStats)
