bc1decccfddcf1e8780112390b2dc56753b95381
chriscauley <chris@lablackey.com>
2021-02-16 00:08:48 +0000

    disco view

---
+++ vue-client/src/views/Disco.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <h1>Disco</h1>
+    <div class="flex">
+      <settings-popper />
+    </div>
+    <render-board v-if="board" :board="board" :hash="hash" />
+  </div>
+</template>
+
+<script>
+import css from '@unrest/css'
+import Board from 'tw/Board'
+import RenderBoard from '@/components/RenderBoard'
+import EventMixin from '@/EventMixin'
+import SettingsPopper from '@/components/SettingsPopper'
+
+const parseBoard = (s) => {
+  const [W, H] = s.split('x').map(Number)
+  return { W, H }
+}
+
+export default {
+  components: { RenderBoard, SettingsPopper },
+  mixins: [EventMixin],
+  __route: {
+    path: '/disco/:board/:pieces/',
+  },
+  data() {
+    return {
+      board: null,
+      hash: null,
+      css,
+    }
+  },
+  mounted() {
+    this.restart()
+  },
+  methods: {
+    restart() {
+      const { params } = this.$route
+      const options = parseBoard(params.board)
+      options.pieces = '+' + params.pieces
+      options.player = 1
+      this.board = new Board(options)
+      this.sync()
+    },
+    sync() {
+      this.hash = this.board.game.turn.toString()
+    },
+    keydown(e) {
+      if (e.key.includes('Arrow')) {
+        this.board.game.pressArrow(e, this.sync)
+      } else if (e.key === ' ') {
+        this.board.game.pressSpace(e, this.sync)
+      }
+    },
+  },
+}
+</script>
--- vue-client/src/views/index.js
+++ vue-client/src/views/index.js
@@ -1,4 +1,5 @@
 import Coloseum from './Coloseum'
+import Disco from './Disco'
 import Home from './Home'
 
-export default { Coloseum, Home }
+export default { Coloseum, Disco, Home }
