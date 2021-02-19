0ec78cf7bdf833a1cff2d517c0548099beacd588
chriscauley <chris@lablackey.com>
2021-02-19 00:47:39 +0000

    move.after.done shortcut

--- tw/move/after.js
+++ tw/move/after.js
@@ -1,10 +1,17 @@
 // Creates a queue of functions to be executed when the move is completed
 // Adds that function to the queue
 
-export default (move, f) => {
+const after = (move, f) => {
   if (!move.after) {
     move.after = []
   }
   move.after.push(f)
   return move
 }
+
+after.done = (move, f) => {
+  move.done = true
+  return after(move, f)
+}
+
+export default after
